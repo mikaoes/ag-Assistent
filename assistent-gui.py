d2242dfb7a05543e6e035ca1da4f893aabb23b9a
Mika <66172894+mikaoes@users.noreply.github.com>
2023-03-14 19:10:17 +0100

    add response display in gui, minor fixes etc. (#8)
    * add core support
    
    * Ich habe Mikas Rechtschreibfehler behoben.
    
    * I habe das Textfeld der GUI erstellt.
    
    * return support (#5)
    
    * minor fixes, add import to assistent-gui
    
    * add return to core request and matchcase in commands()
    
    * remove test leftover
    
    * return support (#5) (#6) (#7)
    
    * minor fixes, add import to assistent-gui
    
    * add return to core request and matchcase in commands()
    
    * remove test leftover
    
    ---------
    
    Co-authored-by: Lasse Thoroe <127113991+lt20202122@users.noreply.github.com>

--- assistent-gui.py
+++ assistent-gui.py
@@ -10,6 +10,10 @@ GroesseY=700
 Red=(255,0,0)
 
 # Funktionen definieren
+def TextAnzeigen():
+  print("Antwort(var):", response)
+  ErgebnisA1.config(text=eingabe)
+  ErgebnisA2.config(text=response)
 def OkClick():
   global eingabe
   eingabe=eingabeF.get()
@@ -17,7 +21,9 @@ def OkClick():
   if eingabe=="":
     print("Bitte gib etwas ein!")
   else:
-    #core.request(eingabe)
+    global response
+    response = core.request(eingabe) # Funktion aus Core gibt Antwort zurück
+    TextAnzeigen()
     print("Else in OkClick ausgeführt")
     nichts2=StringVar()
     nichts2.set("")
@@ -28,7 +34,6 @@ def xBClick():
   nichts.set("")
   print("xB is clicked")
   eingabeF.config(textvariable=nichts)
-  #eingabeF.update()
 def lastTextBClick(eingabe2):
   print("lastTextBClick aufgrufen")
   try:
@@ -54,6 +59,10 @@ lastTextB.place(x=100, y=100, height=40)
 OkB=Button(Fenster, text="Ok", command=OkClick)
 OkB.place(x=386, y=60, height=40, width=30)
 xB=Button(Fenster, text="X", foreground="#000000", background='#ADD8E6', command=xBClick)
+ErgebnisA1=Label(Fenster, text="")
+ErgebnisA2=Label(Fenster, text="")
+ErgebnisA2.place(x=50, y=440, height=220, width=600)
+ErgebnisA1.place(x=50, y=200, height=220, width=600)
 xB.place(x=345, y=65)
 eingabeF.place(x=100, y=60, height=40)
 Anzeige1.pack()
