4c7e76eb6e8794670a3158ca72ebfa7b1b085b0d
Mika <66172894+mikaoes@users.noreply.github.com>
2023-03-20 14:01:55 +0100

    fix; may delete branch (#12)

--- plugins/hallo.py
+++ plugins/hallo.py
@@ -1,9 +1,10 @@
-def greet(name, name2="zwo"):
-    return f"Hello, {name} and {name2}!"
+def greet(name):
+    return f"Hallo {name}!"
 
 def gruesse_von_an(von, an):
     return f"Grüße von {von} an {an}!"
 
 commands = {
     "greet _": greet,
-    "gruesse von _ an _": gruesse_von_an}
+    "gruesse von _ an _": gruesse_von_an
+    }
