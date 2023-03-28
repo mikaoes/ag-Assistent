057c6bc3493964f36853df446956d4a2770c7879
Mika <mika.oesterreich@icloud.com>
2023-03-28 22:18:18 +0200

    add support for output only apps

--- core/__init__.py
+++ core/__init__.py
@@ -1,11 +1,19 @@
 print("core imported")
 
 import os
+import types
 if __name__ == "__main__":
     print("core is main")
-else:
-    import plugins
-    com_list = plugins.command_list()
+    # import package from sibling directory
+    import sys
+    sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+mode = False # mode for app runner
+d_obj = None # object for app runner
+
+import plugins
+com_list = plugins.command_list()
+
 
 
 def command_pal(r): # command palette for system commands
@@ -39,22 +47,25 @@ def plugin_commands(r):
             except IndexError:
                 None
             
-
+    def diff_returner() -> tuple[bool, str]:
+        nonlocal d
+        ret_bool, ret_str_or_class = None, None
+        # if d is function / is class
+        if callable(d[list(d.keys())[0]]):
+            ret_bool, ret_str_or_class = True, d[list(d.keys())[0]](*args(d, r_split))
+        else:
+            ret_bool, ret_str_or_class = False, d[list(d.keys())[0]]
+        return ret_bool, ret_str_or_class
 
     match len(d):
-        case 0: return False, "Command not found."
-        case 1: return True, d[list(d.keys())[0]](*args(d, r_split))
-        case _: return False, "Multiple commands found."
+        case 0: return True, "Command not found.1"
+        case 1: return diff_returner()        #True, str(d[list(d.keys())[0]](*args(d, r_split)))
+        case _: return True, "Multiple commands found."
             
 
 
-def commands(r): # command palette for system commands
-    match r:
-        case "exit": exit()
-        case "clear": os.system("clear"); return True, None
-        case "cls": os.system("cls"); return True, None
-        case "help": return True, "See help at help.md"
-        case _: return False, None
+def app_runner(do, r):
+    print(f"recieved {do} and {r} with type {type(r)}")
 
 os.system("clear") # first clear after import log messages
 
@@ -66,14 +77,16 @@ def loop():
 
 def request(r):
     a = command_pal(r)
-    if a[0]:
+    if a[0]: # if is system command
         return a[1]
     else:
         ap = plugin_commands(r)
-        if ap[0]:
+        if ap[0]: # if is command
             return ap[1]
         else:
-            return "Command not found."
+            return str(ap[1])
+
+
     
 if __name__ == "__main__":
     loop()
--- plugins/hallo.py
+++ plugins/hallo.py
@@ -4,7 +4,24 @@ def greet(name):
 def gruesse_von_an(von, an):
     return f"Grüße von {von} an {an}!"
 
+class funcApp:
+    def __init__(self) -> None:
+        self.c = 0
+    def __str__(self) -> str:
+        f_name = "f" + str(self.c)
+        method = getattr(self, f_name)
+        self.c += 1
+        return str(method())
+    def f0(self):
+        return "works !!!"
+    def f1(self):
+        return "works as well"
+        
+        
+
+
 commands = {
     "greet _": greet,
-    "gruesse von _ an _": gruesse_von_an
+    "gruesse von _ an _": gruesse_von_an,
+    "funcApp": funcApp()
     }
