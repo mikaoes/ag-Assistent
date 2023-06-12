32fdf26ab028bb028a6ad145de4380178202a5c9
userbanane <Bela.kirchner@pmg-meine.de>
2023-06-12 14:55:13 +0200

    fixed non-importable module names

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -5,7 +5,7 @@ import importlib
 def command_list():
     d = dict()
     for i in os.listdir("plugins"):
-        if i.endswith(".py") and i != "__init__.py" and not i.startswith("00piig"):
+        if i.endswith(".py") and i != "__init__.py" and not i.startswith("piig00"):
             # import "commands" dictionary from plugin
             from_path = "plugins." + i[:-3]
             c = importlib.import_module(from_path)
